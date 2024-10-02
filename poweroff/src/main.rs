d04200a62cd993ae60a3ec3c7c381116990c6925
altacountbabi <altacountbabi@users.noreply.github.com>
2024-10-02 22:50:11 +0300

    Complete poweroff & reboot binaries.

--- poweroff/src/main.rs
+++ poweroff/src/main.rs
@@ -1,5 +1,36 @@
 use ipc_init::Init;
+use nix::sys::reboot::{reboot, RebootMode};
+use std::{
+    env,
+    fmt::Display,
+    io::{self, Write},
+};
 
 fn main() {
-    Init::new("/tmp/init/init.sock").poweroff();
+    let args = env::args().collect::<Vec<String>>();
+
+    if args.len() > 1 && args[1] == "-f" {
+        if confirm("Are you sure you want to force power off?", false) {
+            reboot(RebootMode::RB_POWER_OFF).expect("Failed to power off system");
+        }
+    } else {
+        Init::new("/tmp/ipc/init.sock").poweroff();
+    }
+}
+
+fn confirm(prompt: impl Display, default: bool) -> bool {
+    loop {
+        print!("{} [{}]: ", prompt, if default { "Y/n" } else { "y/N" });
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        match input.trim().to_lowercase().as_str() {
+            "y" | "yes" => return true,
+            "n" | "no" => return false,
+            "" => return default,
+            _ => println!("Please enter 'y' or 'n'."),
+        }
+    }
 }
--- reboot/src/main.rs
+++ reboot/src/main.rs
@@ -1,5 +1,36 @@
 use ipc_init::Init;
+use nix::sys::reboot::{reboot, RebootMode};
+use std::{
+    env,
+    fmt::Display,
+    io::{self, Write},
+};
 
 fn main() {
-    Init::new("/tmp/init/init.sock").reboot();
+    let args = env::args().collect::<Vec<String>>();
+
+    if args.len() > 1 && args[1] == "-f" {
+        if confirm("Are you sure you want to force reboot?", false) {
+            reboot(RebootMode::RB_AUTOBOOT).expect("Failed to reboot system");
+        }
+    } else {
+        Init::new("/tmp/ipc/init.sock").reboot();
+    }
+}
+
+fn confirm(prompt: impl Display, default: bool) -> bool {
+    loop {
+        print!("{} [{}]: ", prompt, if default { "Y/n" } else { "y/N" });
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        match input.trim().to_lowercase().as_str() {
+            "y" | "yes" => return true,
+            "n" | "no" => return false,
+            "" => return default,
+            _ => println!("Please enter 'y' or 'n'."),
+        }
+    }
 }
